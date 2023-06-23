edabb5dd27d649db00fa6be942e66f6e01cc62a9
wingsweaver <wingsweaver@163.com>
2023-06-23 10:19:11 +0800

    加入 kuja-starter-common 系的示例代码

---
+++ kuja-common-examples/kuja-common-starter-examples/kuja-starter-common-webflux-test1/src/main/java/com/example/kuja/starter/common/webflux/test1/TestController.java
@@ -0,0 +1,15 @@
+package com.example.kuja.starter.common.webflux.test1;
+
+import com.wingsweaver.kuja.common.boot.returnvalue.ReturnValue;
+import com.wingsweaver.kuja.common.web.controller.AbstractController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController extends AbstractController {
+    @RequestMapping
+    public ReturnValue test() {
+        return this.success();
+    }
+}
---
+++ kuja-common-examples/kuja-common-starter-examples/kuja-starter-common-webmvc-jee-test1/src/main/java/com/example/kuja/starter/common/webmvc/jee/test1/KujaStarterCommonWebMvcJeeTest1Application.java
@@ -0,0 +1,11 @@
+package com.example.kuja.starter.common.webmvc.jee.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KujaStarterCommonWebMvcJeeTest1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(KujaStarterCommonWebMvcJeeTest1Application.class, args);
+    }
+}
